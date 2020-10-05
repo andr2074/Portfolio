b7fd6d8c013d3dba36d6d6f58693b2d4b3bec02e
andr2074 <47871173+andr2074@users.noreply.github.com>
2020-10-06 00:56:00 +0300

    Photo

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 		$('html,body').animate({scrollTop: $(aid).offset().top - 75}, 300);
 	});
 	$(window).scroll(function() {
-		console.log($(window).scrollTop());
 		if ($(window).scrollTop() >= 50) {
 			$('header').addClass('header__white');
 		}
