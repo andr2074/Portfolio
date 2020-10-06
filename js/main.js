6d04803f622d286a7a29378fb1e70f6e33f8903b
andr2074 <47871173+andr2074@users.noreply.github.com>
2020-10-06 13:00:06 +0300

    NEw color

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		$('html,body').animate({scrollTop: $(aid).offset().top - 75}, 300);
 	});
 	$(window).scroll(function() {
-		if ($(window).scrollTop() >= 50) {
+		if ($(window).scrollTop() >= 1) {
 			$('header').addClass('header__white');
 		}
 		else if ($(window).scrollTop() < 50) {
